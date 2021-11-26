b693145a2cfeef4cf40895d772d5b748cad808ab
dapetoo <dapetoo@gmail.com>
2021-11-26 12:32:47 +0100

    Update message

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello Peter!'));
+app.get('/', (req, res) => res.send('Hello Peter, this is a message pushed using CodeCommit and CodePipeline!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
